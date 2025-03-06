b7dfe1d6957d747e8db260c927862d595198bc1d
HungTran284 <trannguyenhung284@gmail.com>
2025-03-06 10:10:26 +0700

    create forget pass

---
+++ my-app/src/app/quen-mat-khau/nhapma/nhapma.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nhapma',
+  imports: [],
+  templateUrl: './nhapma.component.html',
+  styleUrl: './nhapma.component.css'
+})
+export class NhapmaComponent {
+
+}
