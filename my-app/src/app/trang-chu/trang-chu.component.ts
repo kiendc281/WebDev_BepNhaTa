fa1f0cd45ae2129ebd1bd42a6fb345f42532262f
HungTran284 <trannguyenhung284@gmail.com>
2025-03-17 17:18:17 +0700

    done sliderbanner

--- my-app/src/app/trang-chu/trang-chu.component.ts
+++ my-app/src/app/trang-chu/trang-chu.component.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-trang-chu',
   templateUrl: './trang-chu.component.html',
   styleUrls: ['./trang-chu.component.css'],
+  standalone: true,
+  imports: [CommonModule],
 })
 export class TrangChuComponent implements OnInit, OnDestroy {
-  private bannerImages = [
-    '../../assets/trang-chu/banner1.png',
-    '../../assets/trang-chu/banner2.png',
-    '../../assets/trang-chu/banner3.jpg',
+  bannerImages = [
+    'assets/trang-chu/banner1.png',
+    'assets/trang-chu/banner2.png',
+    'assets/trang-chu/banner3.jpg',
   ];
   currentBannerSlide = 0;
-  currentBannerImage = this.bannerImages[0];
   private slideInterval: any;
   private readonly slideDelay = 5000; // 5 seconds
+  private isAnimating = false;
 
   constructor(private router: Router) {}
 
@@ -40,15 +43,48 @@ export class TrangChuComponent implements OnInit, OnDestroy {
   }
 
   nextBannerSlide() {
-    this.currentBannerSlide = (this.currentBannerSlide + 1) % 3;
-    this.currentBannerImage = this.bannerImages[this.currentBannerSlide];
+    if (this.isAnimating) return;
+
+    this.isAnimating = true;
+    this.stopSlideTimer();
+
+    const nextSlide = (this.currentBannerSlide + 1) % this.bannerImages.length;
+    this.currentBannerSlide = nextSlide;
+
+    setTimeout(() => {
+      this.isAnimating = false;
+      this.startSlideTimer();
+    }, 800); // Match transition duration
+  }
+
+  prevBannerSlide() {
+    if (this.isAnimating) return;
+
+    this.isAnimating = true;
+    this.stopSlideTimer();
+
+    const prevSlide =
+      (this.currentBannerSlide - 1 + this.bannerImages.length) %
+      this.bannerImages.length;
+    this.currentBannerSlide = prevSlide;
+
+    setTimeout(() => {
+      this.isAnimating = false;
+      this.startSlideTimer();
+    }, 800); // Match transition duration
   }
 
   setCurrentBannerSlide(index: number) {
+    if (index === this.currentBannerSlide || this.isAnimating) return;
+
+    this.isAnimating = true;
     this.stopSlideTimer();
     this.currentBannerSlide = index;
-    this.currentBannerImage = this.bannerImages[this.currentBannerSlide];
-    this.startSlideTimer();
+
+    setTimeout(() => {
+      this.isAnimating = false;
+      this.startSlideTimer();
+    }, 800); // Match transition duration
   }
 
   navigateToProducts() {
