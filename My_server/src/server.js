d64e81e156d52a57324e9e749d8cfd5f2fdb6494
kien doan <caokienzxc@gmail.com>
2025-03-20 16:12:12 +0700

    thêm 2 dòng limit ở config cors

--- My_server/src/server.js
+++ My_server/src/server.js
@@ -30,7 +30,8 @@ if (!process.env.EMAIL_USER || !process.env.EMAIL_PASSWORD) {
 
 // config cors
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // Khai báo route
 app.use('/api', accountRoutes);
